67405916c1bd15ad64c3c52d136c7c1e4110453c
Tue Dang <dangtritue@gmail.com>
2018-12-13 02:31:30 +0700

    add tramdoc.vn

--- js/extract_link_by_selector.js
+++ js/extract_link_by_selector.js
@@ -1,38 +1,34 @@
 // http://groups.google.com/group/mozilla.dev.tech.dom/browse_thread/thread/7ecbbb066ff2027f
 // Martin Honnen
 //  http://JavaScript.FAQTs.com/ 
-(function()
-{
-	function getLinks(selector, rawHTML)
-	{
-		var doc = document.createElement("html");
-		doc.innerHTML = rawHTML;
-		// var links = doc.getElementsByTagName("a");
-		var links = doc.querySelectorAll(selector);
-		var titles = [];
-		var urls = [];
-		for (var i=0; i<links.length; i++) {
-			if(links[i].href.length===0) {
-				continue;
-			}
-			urls.push(links[i].href);
-            titles.push(links[i].innerText.replace(/"/g, '&quot;'));
-		}
-		return {"urls" : urls, "titles": titles};
-	}
-	(function sendLinksMessage()
-	{
+(function () {
+    function getLinks(selector, rawHTML) {
+        var doc = document.createElement("html");
+        doc.innerHTML = rawHTML;
+        var links = doc.querySelectorAll(selector);
+        var titles = [];
+        var urls = [];
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].href.length === 0) {
+                continue;
+            }
+            urls.push(links[i].href.trim());
+            titles.push(links[i].innerText.replace(/"/g, '&quot;').trim());
+        }
+        return {"urls": urls, "titles": titles};
+    }
+
+    (function sendLinksMessage() {
         BodyCopy = document.body.cloneNode(true);
         var selection = document.body;
-		var div = document.createElement('div');
-		div.appendChild(selection);
-		var vs=div.innerHTML;
-		var req = getLinks(selector, vs);
-		//console.log(req);
-		if(req.urls.length > 0)
-		{
-			chrome.runtime.sendMessage(req);
-		}
+        var div = document.createElement('div');
+        div.appendChild(selection);
+        var vs = div.innerHTML;
+        var req = getLinks(selector, vs);
+        //console.log(req);
+        if (req.urls.length > 0) {
+            chrome.runtime.sendMessage(req);
+        }
         document.body = BodyCopy;
-	})();
+    })();
 })();
